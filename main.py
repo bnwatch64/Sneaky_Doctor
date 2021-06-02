621027f434a23d49b75f386e78c219e51d4a685e
TAsUjxnMIL <sujan_10@hotmail.de>
2021-06-02 12:25:57 +0200

    User Interface updated Background sound added

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from gameConstants import *
 
 
 # TODO:
+# Docstrings in all files, functions, classes
+# Instead of print() use log file with important log messages
+
 pygame.init()
 screenSize = [GAME_SIZE[0], GAME_SIZE[1] + 2 * BAR_HEIGHT]
 pygame.display.set_mode(screenSize)
